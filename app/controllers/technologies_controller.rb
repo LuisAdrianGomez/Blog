c574c4ba398507395548cbe47ec9778419c50173
Luis Adrián Gómez Elizondo <luiis_gomez_93@hotmail.com>
2015-05-14 13:20:31 -0500

    CRUD technologies finished & some things added
    - CRUD technologies finished
    - strong params
    - partial view form

--- app/controllers/technologies_controller.rb
+++ app/controllers/technologies_controller.rb
@@ -9,30 +9,47 @@ class TechnologiesController < ApplicationController
   def show
   	@technology = Technology.find(params[:id])
   end
+
   #GET /technologies/new
   def new
   	@technology = Technology.new
   end
+
   #POST /technologies
   def create
-  	@technology = Technology.new({ name: params[:technology][:name],
-								  	description: params[:technology][:description],
-								  	version: params[:technology][:version],
-									link: params[:technology][:link]})
+  	@technology = Technology.new(technology_params)
   	if @technology.save
-  		redirect_to @technology
+  		redirect_to technologies_path
   	else
   		render :new
   	end
   end
+
+  #DELETE /technologies/:id
   def destroy
   	@technology = Technology.find(params[:id])
   	@technology.destroy
   	redirect_to technologies_path
   end
+
+  def edit 
+    @technology = Technology.find(params[:id])
+  end
+
   #PUT technologies/:id
   def update
-    
+    @technology = Technology.find(params[:id])
+    if @technology.update(technology_params)
+      redirect_to technologies_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def technology_params
+    params.require(:technology).permit(:name,:description,:version,:link)
   end
 
 
